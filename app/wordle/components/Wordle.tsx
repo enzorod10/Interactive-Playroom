74631ea90e3c6a4e1400e8a03d9bff0fdb194c1b
enzo <me@enzorod.com>
2024-10-09 03:11:41 -0400

    moved wordle context wrapper location

--- app/wordle/components/Wordle.tsx
+++ app/wordle/components/Wordle.tsx
@@ -1,12 +1,15 @@
 import Grid from "./Grid/Grid";
 import Keyboard from "./Keyboard";
+import { WordleWrapper } from "../context/WordleContext";
 
 const Wordle = () => {
     return (
-        <div className="flex flex-col h-[calc(100dvh-48px)] sm:gap-10 sm:mt-10 lg:h-fit lg:gap-10 lg:my-10" >
-            <Grid/>
-            <Keyboard/>
-        </div>
+        <WordleWrapper>
+            <div className="flex flex-col h-[calc(100dvh-48px)] sm:gap-10 sm:mt-10 lg:h-fit lg:gap-10 lg:my-10" >
+                <Grid/>
+                <Keyboard/>
+            </div>
+        </WordleWrapper>
     )
 }
 
--- app/wordle/page.tsx
+++ app/wordle/page.tsx
@@ -1,11 +1,8 @@
 import Wordle from "./components/Wordle";
-import { WordleWrapper } from "./context/WordleContext";
 
 export default function Home() {
     return (
-      <WordleWrapper>
         <Wordle/>
-      </WordleWrapper>
     );
   }
   
